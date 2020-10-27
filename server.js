b1c361a331c6f1c66d2c428b00ae0f63da9a2eb9
unknown <samuellyworld@gmail.com>
2020-10-27 16:55:06 -0700

    fixing root route

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req,res) => {
-	res.json(database.users);
+	res.json('it is working');
 });
 
 app.post('/signin', (req,res) => {signin.handleSignin(req,res, bcrypt, db)});
