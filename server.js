cfa3e15e294e2572d3987a75422ef3fcbc58c35d
unknown <samuellyworld@gmail.com>
2020-10-27 18:19:54 -0700

    adding'

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image.js');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-cylindrical-91672',
     user : 'postgres',
     password : 'aspirin',
     database : 'smart-brain'
